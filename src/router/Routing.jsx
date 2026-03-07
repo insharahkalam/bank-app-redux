7b33144fe899ca5c08901693306be19c0ac3b9a0
insharahkalam <insharahkalam19@gmail.com>
2026-03-07 08:16:29 +0500

    updated

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Dashboard from "../Pages/dashboard"
+import Dashboard from "../Pages/Dashboard"
 import Home from "../Pages/Home"
 
 const Routing = () => {
